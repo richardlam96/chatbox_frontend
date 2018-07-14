2e512248d7450452e75aafbd38cec5f48726adc9
Richard Lam <richardlam78@huntermail.cuny.edu>
2018-07-14 18:47:31 -0400

    nevermind, AuthRoute still does not work

--- src/utils/AuthRoute.js
+++ src/utils/AuthRoute.js
@@ -3,11 +3,15 @@ import { Route, Redirect } from 'react-router-dom';
 
 
 const AuthRoute = ({ component: Component, exact, currentUser, path }) => (
-  <Route exact={exact} path={path} render={props => (
-    currentUser.isAuthenticated
-      ? <Component {...props} />
-      : <Redirect to="/signin" />
-  )} />
+  <Route 
+    exact={exact} 
+    path={path} 
+    render={props => (
+      currentUser.isAuthenticated
+        ? <Component {...props} />
+        : <Redirect to="/signin" />
+    )} 
+  />
 );
 
 
