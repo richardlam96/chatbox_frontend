568634fd262677ac1527d3587edab97ddce6fcc5
Richard <richardlam96@gmail.com>
2018-08-01 16:32:35 -0400

    connecting to io through server and channel

--- src/components/ChatboxComponent.js
+++ src/components/ChatboxComponent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import io from 'socket.io-client';
 import '../styles/ChatboxComponent.css';
 
-const socket = io('http://localhost:3001');
 
 
 class ChatboxComponent extends Component {
@@ -10,28 +9,38 @@ class ChatboxComponent extends Component {
 		super(props);
     this.state = {
       message: '',
-			socketTest: '',
+			messages: [],
     };
 
 		// Socket to listen to broadcasts.
-		socket.on('send', msg => {
+		let { 
+			match: { params },
+		} = this.props;
+
+		let url = 'http://localhost:3001/' + params.serverId + '/' + params.channelId; 
+		console.log(url);
+		// let url = 'http://localhost:3001/555555/555555';
+		this.socket = io(url);
+		this.socket.on('send', msg => {
 			this.setState({
-				socketTest: msg,
+				messages: [
+					...this.state.messages,
+					msg.text,
+				],
 			});
 		});
 	}
 
 	componentDidMount() {
-		let {
-			currentUser,
-			indexMessages,
-			match: { params },
-		} = this.props;
+		// let {
+		// 	currentUser,
+		// 	indexMessages,
+		// 	match: { params },
+		// } = this.props;
 
-		// For handling refreshes
-		indexMessages(currentUser.id, params.serverId, params.channelId);
-
-}
+		// // For handling refreshes
+		// indexMessages(currentUser.id, params.serverId, params.channelId);
+	}
 
 	handleChange = e => {
 		this.setState({
@@ -41,9 +50,18 @@ class ChatboxComponent extends Component {
 
 	handleCreateMessage = (e) => {
 		e.preventDefault();
+		 let {
+			 currentUser,
+			 match: { params} ,
+		 } = this.props;
 
 		// Emit message for real time functionality.
-		socket.emit('send', this.state.message);
+		this.socket.emit('send', {
+			text: this.state.message,
+			user: currentUser.username,
+			server: params.serverId,
+			channel: params.channelId,
+		});
 
 		// let {
 		// 	currentUser,
@@ -64,35 +82,44 @@ class ChatboxComponent extends Component {
 	}
 
 	render() {
-		let {
-			currentUser,
-			channelsById,
-			messagesById,
-			match: { params },
-		} = this.props;
+		// let {
+		// 	currentUser,
+		// 	channelsById,
+		// 	messagesById,
+		// 	match: { params },
+		// } = this.props;
 
-		let currentChannel = channelsById[params.channelId];
-    let messageList = [];
-    if (currentChannel && currentChannel.messages) {
-      messageList = currentChannel.messages.map(messageId => {
-				let message = messagesById[messageId];
-        if (message) {
-          return (
-						<li key={messageId} className="chatbox-message">
-							<div className="chatbox-message-details">
-								{currentUser.username}
-							</div>
-							<p>{message.text}</p>
-						</li>
-					);
-        }
-      });
-    }
+		// let currentChannel = channelsById[params.channelId];
+    // let messageList = [];
+    // if (currentChannel && currentChannel.messages) {
+    //   messageList = currentChannel.messages.map(messageId => {
+		// 		let message = messagesById[messageId];
+    //     if (message) {
+    //       return (
+		// 				<li key={messageId} className="chatbox-message">
+		// 					<div className="chatbox-message-details">
+		// 						{currentUser.username}
+		// 					</div>
+		// 					<p>{message.text}</p>
+		// 				</li>
+		// 			);
+    //     }
+    //   });
+    // }
+		
+		let messageList = this.state.messages.map((message, idx) => (
+			<li key={idx} className="chatbox-message">
+				<div className="chatbox-message-details">
+				</div>
+				<p>{message}</p>
+			</li>
+		));
+
 		
 		return (
 			<div className="chatbox-component">
         <div className="chatbox-header">
-          <p>Chatbox on {params.channelId}</p>
+          <p>Chatbox on {this.props.match.params.channelId}</p>
 					<p>{this.state.socketTest}</p>
         </div>
 
