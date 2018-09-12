ea582ada25509c7c6dddc428f694e6d7c4a75991
Richard <richardlam96@gmail.com>
2018-09-12 16:58:36 -0400

    removed service worker registration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 import './styles/index.css';
 
+unregister();
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
