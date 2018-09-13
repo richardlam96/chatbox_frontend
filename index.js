fcf531af16710ee9e7ab21978d7ed01a1342436d
Richard Lam <richardlam96@gmail.com>
2018-09-13 10:08:00 -0400

    added .env file and updated .gitignore

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
+app.listen(process.env.PORT || 3001, process.env.IP, () => {
 	console.log('Discord Clone started.');
 });
