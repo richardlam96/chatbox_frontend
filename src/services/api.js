2007b3960486678a52a49e29928fd379f12e7b48
Richard <richardlam96@gmail.com>
2018-09-13 15:53:52 -0400

    commit to trigger rebuild after running npm build script

--- src/services/api.js
+++ src/services/api.js
@@ -7,23 +7,23 @@ const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 export function apiCall(method, path, data=undefined) {
 	const authToken = "Bearer " + localStorage.getItem('jwtToken');
   console.log('SERVER', process.env.REACT_APP_SERVER_URL);
-	return fetch(SERVER_URL + path, {
-		method,
-		headers: {
-			"Content-Type": "application/json; charset=utf-8",
-			"Authorization": authToken,
-		},
-		body: JSON.stringify(data),
-	}).then(response => {
-		return response.json();
-	}).then(data => {
-		// If there is no error, there will not be a status code
-		if (data.status !== 200 && data.status !== undefined) {
-			throw new Error(data.message);
-		}
-		return data;
-	}).catch(error => {
-		throw new Error(error.message);
-	});
+  return fetch(SERVER_URL + path, {
+    method,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Authorization": authToken,
+    },
+    body: JSON.stringify(data),
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    // If there is no error, there will not be a status code
+    if (data.status !== 200 && data.status !== undefined) {
+      throw new Error(data.message);
+    }
+    return data;
+  }).catch(error => {
+    throw new Error(error.message);
+  });
 }
-		
+
