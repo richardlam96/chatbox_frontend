15c5fd7c2ec4de31abae2745dd6a5a7dccddfc64
Richard Lam <richardlam96@gmail.com>
2018-09-26 16:34:58 -0400

    suite works (updated chrome and added timeout)

--- src/tests/landing.js
+++ src/tests/landing.js
@@ -1,17 +1,18 @@
-const { Builder, By, until } = require('selenium-webdriver');
+const { Builder, By, until, Capabilities } = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const chrome = require('selenium-webdriver/chrome');
 const { assert } = require('chai');
 
 /* Test the Landing Page for proper elements */
-
+// This works for firefox and chrome if Capabilities are set separately.
+// (Just had to update google-chrome-stable).
 describe('Landing Page', function() {
   let driver;
   this.timeout(60000);
   
   before(function() {
     driver = new Builder()
-    .forBrowser('firefox')
+    .withCapabilities(Capabilities.chrome())
     .build();
 
     return driver.get('http://127.0.0.1:3001');
--- src/tests/landing_suite.js
+++ src/tests/landing_suite.js
@@ -2,20 +2,26 @@ const { Builder, By, until } = require('selenium-webdriver');
 const { suite } = require('selenium-webdriver/testing');
 const { assert } = require('chai');
 
+
 suite(function(env) {
   describe('Landing Page', function() {
     let driver;
+    this.timeout(60000);
 
-    before(async function() {
-      driver = await env.builder().build();
+    before(function() {
+      driver = env.builder().build();
+      return driver.get('http://127.0.0.1:3001');
     });
 
-    after(() => driver.quit());
+    after(function() {
+      driver.quit();
+    });
 
-    it('should have the title, Chatbox App', async function() {
-      await driver.get('http://127.0.0.1:3001');
-      let title = await driver.getTitle();
-      assert.equal(title, 'Chatbox App');
+    it('should have the title, Chatbox App', function() {
+      return driver.getTitle()
+      .then(title => {
+        assert.equal(title, 'Chatbox App');
+      })
     });
   });
 
